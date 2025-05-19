56b534abeca0c4146e1eb7760d424d2b3f0c7e36
hdraux <h.draux@digital-science.com>
2025-05-19 21:36:58 +0100

    Update facetMappings.js

--- src/utils/facetMappings.js
+++ src/utils/facetMappings.js
@@ -1,15 +1,8 @@
-export const PUBLICATION_FACET_TO_DSL = {};
-export const GRANT_FACET_TO_DSL = {};
-export const DATASET_FACET_TO_DSL = {};
-export const PATENT_FACET_TO_DSL = {};
-export const CLINICAL_TRIAL_FACET_TO_DSL = {};
-export const POLICY_DOCUMENT_FACET_TO_DSL = {};
-
-export const DSL_MAPPING = {
-  publications: PUBLICATION_FACET_TO_DSL,
-  grants: GRANT_FACET_TO_DSL,
-  datasets: DATASET_FACET_TO_DSL,
-  patents: PATENT_FACET_TO_DSL,
-  clinical_trials: CLINICAL_TRIAL_FACET_TO_DSL,
-  policy_documents: POLICY_DOCUMENT_FACET_TO_DSL
+export const FACET_TO_DSL = {
+  publications: { year: "year", open_access_status: "open_access" },
+  grants: { year: "start_year", funder: "funders" },
+  patents: { year: "year", granted_year: "granted_year" },
+  clinical_trials: { year: "year", active_year: "active_years" },
+  policy_documents: { year: "year", research_org: "publisher_org" },
+  datasets: { year: "year", researcher: "researchers" }
 };
